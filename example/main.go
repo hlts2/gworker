0eac5efd4bcc40485abe50dfcea6d21cc11706c9
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-06-26 14:00:51 +0900

    added base processing for worker and example code

---
+++ example/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/hlts2/gworker"
+)
+
+func main() {
+	d := gworker.NewDispatcher(3)
+	d.StartJobObserver()
+
+	for i := 0; i < 50; i++ {
+		d.Add(func() error {
+			time.Sleep(time.Second * 4)
+			return errors.New("errors.New")
+		})
+	}
+
+	time.Sleep(time.Second * 10)
+
+	d.UpScale(100)
+
+END_LOOP:
+	for {
+		select {
+		case err := <-d.JobError():
+			fmt.Println(err)
+		case _ = <-d.Finish():
+			break END_LOOP
+		}
+	}
+}
--- gworker.go
+++ gworker.go
@@ -1,1 +1,120 @@
 package gworker
+
+import (
+	"sync"
+)
+
+const (
+	defaultWorkerCount = 3
+	maxJobCount        = 100000
+)
+
+type (
+	// Dispatcher ...
+	Dispatcher struct {
+		wg          *sync.WaitGroup
+		mu          *sync.Mutex
+		workers     []*worker
+		workerCount int
+		jobs        chan func() error
+		joberr      chan error
+		finish      chan struct{}
+	}
+
+	worker struct {
+		dispatcher *Dispatcher
+	}
+)
+
+// NewDispatcher returns Dispatcher object
+func NewDispatcher(workerCount int) *Dispatcher {
+	if workerCount < 1 {
+		workerCount = defaultWorkerCount
+	}
+
+	d := &Dispatcher{
+		wg:          new(sync.WaitGroup),
+		mu:          new(sync.Mutex),
+		workers:     make([]*worker, workerCount),
+		workerCount: workerCount,
+		jobs:        make(chan func() error, maxJobCount),
+		joberr:      make(chan error, 1),
+		finish:      make(chan struct{}, 1),
+	}
+
+	for i := 0; i < workerCount; i++ {
+		worker := &worker{
+			dispatcher: d,
+		}
+
+		d.workers[i] = worker
+	}
+
+	d.start()
+
+	return d
+}
+
+// Start starts workers
+func (d *Dispatcher) start() {
+	for _, worker := range d.workers {
+		go worker.start()
+	}
+}
+
+// Add adds job
+func (d *Dispatcher) Add(job func() error) {
+	d.wg.Add(1)
+	d.jobs <- job
+}
+
+// StartJobObserver ...
+func (d *Dispatcher) StartJobObserver() {
+	go func() {
+		for {
+			if len(d.jobs) > 1 {
+				d.wg.Wait()
+				d.finish <- struct{}{}
+				return
+			}
+		}
+	}()
+}
+
+// UpScale ...
+func (d *Dispatcher) UpScale(workerCount int) {
+	if workerCount < 1 {
+		workerCount = defaultWorkerCount
+	}
+
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	for i := 0; i < workerCount; i++ {
+		worker := &worker{
+			dispatcher: d,
+		}
+
+		d.workerCount++
+		go worker.start()
+	}
+}
+
+// JobError returns job error on channel
+func (d *Dispatcher) JobError() <-chan error {
+	return d.joberr
+}
+
+// Finish returns channel when all jobs are finished
+func (d *Dispatcher) Finish() <-chan struct{} {
+	return d.finish
+}
+
+func (w *worker) start() {
+	for job := range w.dispatcher.jobs {
+		go func(err error) {
+			w.dispatcher.joberr <- err
+			w.dispatcher.wg.Done()
+		}(job())
+	}
+}
