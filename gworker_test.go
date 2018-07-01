a1eef0023e79e90f3406af5748e542abb4ce8fa1
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-07-02 03:51:02 +0900

    added test case for worker

--- gworker_test.go
+++ gworker_test.go
@@ -33,38 +33,69 @@ func TestNewDispatcher(t *testing.T) {
 		if d == nil {
 			t.Errorf("tests[%d] - NewDispatcher is nil.", i)
 		}
-
-		got := d.workerCount
-
-		if test.expected != got {
-			t.Errorf("tests[%d] - worker count is wrong. expected: %v, got: %v", i, test.expected, got)
-		}
 	}
 }
 
 func TestStart(t *testing.T) {
-	workerCount := 10
+	test := struct {
+		workerCount int
+		expected    int
+	}{
+		workerCount: 10,
+		expected:    10,
+	}
 
-	d := NewDispatcher(workerCount)
+	d := NewDispatcher(test.workerCount)
 	d.Start()
 
 	got := runtime.NumGoroutine() - 2
-	if workerCount != got {
-		t.Errorf("Starting worker count is wrong. expected: %v, got: %v", workerCount, got)
+	if test.expected != got {
+		t.Errorf("Starting worker count is wrong. expected: %v, got: %v", test.expected, got)
+	}
+
+	d.Stop()
+}
+
+func TestStop(t *testing.T) {
+	test := struct {
+		workerCount int
+		expected    int
+	}{
+		workerCount: 5,
+		expected:    0,
+	}
+
+	d := NewDispatcher(test.workerCount)
+	d.Start()
+
+	d.Stop()
+
+	got := runtime.NumGoroutine() - 2
+	if test.expected != got {
+		t.Errorf("Stop worker count is wrong. expected: %v, got: %v", test.expected, got)
 	}
 }
 
-// func TestUpScale(t *testing.T) {
-// 	workerCount := 5
-// 	d := NewDispatcher(workerCount)
-// 	d.Start()
-//
-// 	d.UpScale(10)
-//
-// 	upScaledWorkerCount := 15
-//
-// 	got := runtime.NumGoroutine() - 2
-// 	if upScaledWorkerCount != got {
-// 		t.Errorf("Upscale is wrong. expected: %v, got: %v", upScaledWorkerCount, got)
-// 	}
-// }
+func TestUpScale(t *testing.T) {
+	test := struct {
+		workerCount  int
+		upScaleCount int
+		expected     int
+	}{
+		workerCount:  10,
+		upScaleCount: 5,
+		expected:     15,
+	}
+
+	d := NewDispatcher(test.workerCount)
+	d.Start()
+
+	d.UpScale(test.upScaleCount)
+
+	got := runtime.NumGoroutine() - 2
+	if test.expected != got {
+		t.Errorf("Upscale is wrong. expected: %v, got: %v", test.upScaleCount, got)
+	}
+
+	d.Stop()
+}
