450a04ff3f8614dd2c47bd3eb86ffdcf02fe3d02
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-07-01 18:49:10 +0900

    added test case to start worker

--- gworker_test.go
+++ gworker_test.go
@@ -1,6 +1,7 @@
 package gworker
 
 import (
+	"runtime"
 	"testing"
 )
 
@@ -30,13 +31,25 @@ func TestNewDispatcher(t *testing.T) {
 	for i, test := range tests {
 		d := NewDispatcher(test.workerCount)
 		if d == nil {
-			t.Errorf("tests[%d] - NewDispatcher is nil", i)
+			t.Errorf("tests[%d] - NewDispatcher is nil.", i)
 		}
 
 		got := d.workerCount
 
 		if test.expected != got {
-			t.Errorf("tests[%d] - worker count is wrong: expected: %v, got: %v", i, test.expected, got)
+			t.Errorf("tests[%d] - worker count is wrong. expected: %v, got: %v", i, test.expected, got)
 		}
+	}
+}
+
+func TestStart(t *testing.T) {
+	workerCount := 10
+
+	d := NewDispatcher(workerCount)
+	d.Start()
+
+	got := runtime.NumGoroutine() - 2
+	if workerCount != got {
+		t.Errorf("Starting worker count is wrong. expected: %v, got: %v", workerCount, got)
 	}
 }
